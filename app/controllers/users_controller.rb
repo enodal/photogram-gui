bbfcb423d906a71c7ba6d425842c1644db3202f8
enodal <164991691+enodal@users.noreply.github.com>
2024-05-07 08:09:26 +0000

    done man

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+ 
+    @list_of_users = matching_users.order({ :username => :asc})
+    render({ :template => "user_templates/index"})
+  end
+  def show
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username})
+    @the_user = matching_usernames.at(0)
+  
+    render ({ :template => "user_templates/show"})
+  
+  end
+  def homepage
+    redirect_to("/users")
+  end
+  def create
+    input_user = params.fetch("input_username")
+    
+    
+      a_new_user = User.new
+      a_new_user.username = input_user   
+      a_new_user.save
+      redirect_to("/users/" + a_new_user.username)
+    end
+    
+    def update
+      the_id = params.fetch("modify_id")
+      matching_users = User.where({:id => the_id})
+      the_user = matching_users.at(0)
+    
+      input_user = params.fetch("input_username")
+      the_user.username = input_user
+    
+      the_user.save
+    
+      redirect_to("/users/" +the_user.username)
+    end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+get("/users", { :controller => "users", :action => "index" })
+get("/users/:path_username", { :controller => "users", :action => "show" })
+get("/photos", { :controller => "photos", :action => "index" })
+get("/photos/:path_id", { :controller => "photos", :action => "show" })
+get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii" })
+post("/insert_photo", { :controller => "photos", :action => "create" })
+post("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
+post("/add_comment", { :controller => "photos", :action => "add" })
+get("/", { :controller => "users", :action => "homepage" })
+post("/add_user", { :controller => "users", :action => "create" })
 
+post("/update_user/:modify_id", { :controller => "users", :action => "update" })
 end
